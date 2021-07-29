65db60e72b2fb85599ef4e04d085270ae62d3031
extrovert <349173537@qq.com>
2021-07-29 08:18:34 +0800

    port

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,7 @@ export function request(config) {
   //这里其实没必要写promise，因为本身返回的就是promise类的，没必要再封装
     //创建axios实例
     const instance = axios.create({
-      baseURL: 'http://152.136.185.210:7878/api/m5',
+      baseURL: 'from coderwhy',
       timeout: 5000
     })
 //axios拦截器，请求拦截的作用
